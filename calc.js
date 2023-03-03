cc181145c271291e53ad16157427d28fc04121a9
Asaaga Simon <simonasarga@gmail.com>
2023-03-03 14:39:24 +0100

    adding comment to js file

--- calc.js
+++ calc.js
@@ -1,3 +1,4 @@
+// import all of html elements
 const previousInput = document.querySelector('[previousInput]');
 const currentInput = document.querySelector('[currentInput]');
 const sqr = document.querySelector('[sqr]');
@@ -8,16 +9,20 @@ const operators = document.querySelectorAll('[opp]');
 const del = document.querySelector('[del]');
 let numbers = document.querySelectorAll('[num]');
 
+// declare a calculator class
 class Calcultor {
+	// declare a constructor
 	constructor(previousInput, currentInput) {
 		this.previousInput = previousInput;
 		this.currentInput = currentInput;
 		this.clear();
 	}
+	// declear a function that calculate the square root
 	sqroot(symbol){
 		this.previousValue = `${symbol}${this.currentValue}`;
 		this.currentValue = Math.sqrt(this.currentValue);
 	}
+	// declear a function that calculate the factorial
 	factorial(symbol) {
  	let current = 1;
  	for(let i = 1; i <= parseInt(this.currentValue); i++){
@@ -26,11 +31,13 @@ class Calcultor {
  	this.previousValue = `${this.currentValue} ${symbol}`
  	this.currentValue = current;
 	}
+	// this function handles the clear button
 	clear() {
 		this.currentValue = '';
 		this.previousValue = '';
 		this.operator = undefined;
 	}
+	// this function add opperator to the screen
 	AppendOperators(operator){
 		if(this.currentValue === '') return;
 		if(this.previousValue !== '') {
@@ -40,17 +47,21 @@ class Calcultor {
 		this.previousValue = `${this.currentValue} ${operator}`
 		this.currentValue = '';
 	}
+	// this function add opperand to screen
 	addNum(operand) {
 		if(operand === '.' && this.currentValue.includes('.')) return
 		this.currentValue = this.currentValue.toString() + operand.toString();	
 	}
+	// this display anything you click on screen
 	display() {
 		this.currentInput.textContent = this.currentValue;	
 		this.previousInput.textContent = this.previousValue;
 	}
+	// this function handle the delete button
 	delete(){
 		this.currentValue = this.currentValue.toString().slice(0,-1);
 	}
+	// this function handles all the calculations
 	calculate(){
 		let result;
 		const current = parseInt(this.currentValue);
@@ -83,24 +94,28 @@ class Calcultor {
 		this.previousValue = '';
 	}
 }
+
+// instantiate the classs
 const obj = new Calcultor(previousInput, currentInput);
 
+// loop through the number buttons
 for(let number of numbers) {
 	number.addEventListener('click', () => {
 		obj.addNum(number.textContent)
 		obj.display()
 	})
 }
+// loop through the operator buttons
 for(let operator of operators) {
 	operator.addEventListener('click', () => {
 		obj.AppendOperators(operator.textContent);
 		obj.display()
 	})
 }
+// we call all the function down here
 fac.addEventListener('click', () => {
 	obj.factorial(fac.textContent);
 	obj.display();
-	console.log('this is living')
 })
 
 clr.addEventListener('click', () => {
